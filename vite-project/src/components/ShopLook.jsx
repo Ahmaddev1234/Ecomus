9b976d6f2d3ef4bcdca731070455910987878edb
ahmad <ahmadhassan282013@gmail.com>
2025-07-16 19:48:49 +0500

    Fix image paths and deploy assets correctly

--- vite-project/src/components/ShopLook.jsx
+++ vite-project/src/components/ShopLook.jsx
@@ -1,25 +1,27 @@
 import React, { useEffect, useRef } from "react";
 import gsap from "gsap";
-import { ScrollTrigger } from "gsap/ScrollTrigger"; 
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 
 
 
 function ShopLook() {
     gsap.registerPlugin(ScrollTrigger);
-    const textRef=useRef(null);
-    
+    const textRef = useRef(null);
 
-    useEffect(()=>{
-        if(textRef.current.children){
-        gsap.fromTo(textRef.current.children,{y:100,opacity:0},{y:0,opacity:1,duration: 0.8,delay:0.1,stagger:0.4,scrollTrigger:{
-            trigger:textRef.current,
-           
-            start:"top 80%",
-            end:"top 30%",             
-            
-            }})
+
+    useEffect(() => {
+        if (textRef.current.children) {
+            gsap.fromTo(textRef.current.children, { y: 100, opacity: 0 }, {
+                y: 0, opacity: 1, duration: 0.8, delay: 0.1, stagger: 0.4, scrollTrigger: {
+                    trigger: textRef.current,
+
+                    start: "top 80%",
+                    end: "top 30%",
+
+                }
+            })
         }
-    },[])
+    }, [])
 
 
     return (
@@ -29,8 +31,13 @@ function ShopLook() {
                 <p className="md:text-md md:w-[100%] w-[80%] text-sm text-center">Inspire and let yourself be inspired, from one unique fashion to another</p>
             </div>
             <div className="w-[100%] md:h-[450px] h-[250px] flex mt-11">
-            <img className="w-[50%] object-cover" src="/images/ShopLook2.jpg" alt="" />
-            <img className="w-[50%] object-cover" src="/images/ShopLook1.jpg" alt="" />
+                <img className="w-[50%] object-cover" src="/images/ShopLook2.jpg" alt="" />
+                <img
+                    className="w-[50%] object-cover"
+                    src="/images/ShopLook1.jpg"
+                    alt="Shop Look"
+                />
+
             </div>
         </div>
     );
