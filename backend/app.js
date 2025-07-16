9aa3c8f7529c9fdb5bd378bb1a39431658d369d2
ahmad <ahmadhassan282013@gmail.com>
2025-07-16 22:08:24 +0500

    Update CORS for deployed frontend

--- backend/app.js
+++ backend/app.js
@@ -1,32 +1,41 @@
-const express=require('express');
-var cors = require('cors')
-const app=express();
-const connection=require('./config/db_connection');
-const users=require("./routes/userRoutes")
-const owners=require("./routes/ownerRoutes")
-const products=require("./routes/productRoutes")
-const cookieParser = require("cookie-parser");
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const connection = require('./config/db_connection');
+const users = require('./routes/userRoutes');
+const owners = require('./routes/ownerRoutes');
+const products = require('./routes/productRoutes');
+const cookieParser = require('cookie-parser');
 const dotenv = require('dotenv');
 dotenv.config();
 
 
+const PORT = process.env.PORT || 3000;
+
+
+const allowedOrigins = [
+  "http://localhost:5173",  
+  "https://ecomus-tan.vercel.app/", 
+];
+
 app.use(cors({
-    origin: "http://localhost:5173",  
-    credentials: true  
-  }))
+  origin: allowedOrigins,
+  credentials: true,
+}));
 
-
-
-app.use(cookieParser())
+app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 
-app.use("/users",users);
-app.use("/products",products);
-app.use("/owners",owners);
+app.use("/users", users);
+app.use("/products", products);
+app.use("/owners", owners);
 
 
-app.listen(3000);
+connection();
 
 
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
